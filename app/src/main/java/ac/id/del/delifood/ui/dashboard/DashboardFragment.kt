9b2e6372e15288ab63f30f42ee87e8d0e9be8565
TrivenaPanjaitan <trivenapanjaitan7.1.3@gmail.com>
2022-05-09 16:24:44 +0700

    Initial Commit

---
+++ app/src/main/java/ac/id/del/delifood/ui/dashboard/DashboardFragment.kt
@@ -0,0 +1,52 @@
+package ac.id.del.delifood.ui.dashboard
+
+import ac.id.del.delifood.R
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import ac.id.del.delifood.databinding.FragmentDashboardBinding
+import androidx.navigation.fragment.findNavController
+
+class DashboardFragment : Fragment() {
+
+    private var _binding: FragmentDashboardBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val dashboardViewModel =
+            ViewModelProvider(this).get(DashboardViewModel::class.java)
+
+        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textDashboard
+        dashboardViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+
+        return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonAdd.setOnClickListener {
+            findNavController().navigate(R.id.action_DashboardFragment_to_AddFragment)
+        }
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
