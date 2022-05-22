d6c3d6b73ad59967ad13230035bb8b849660bbba
TrivenaPanjaitan <trivenapanjaitan7.1.3@gmail.com>
2022-05-22 20:31:23 +0700

    Make black page

--- app/src/main/java/ac/id/del/delifood/db/RecipeDao.kt
+++ app/src/main/java/ac/id/del/delifood/db/RecipeDao.kt
@@ -1,21 +1,19 @@
 package ac.id.del.delifood.db
 
-import androidx.room.Dao
-import androidx.room.Delete
-import androidx.room.Insert
-import androidx.room.Query
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface RecipeDao {
     @Query("SELECT * FROM recipe")
-    suspend fun getAllRecipes(): List<Recipe>
+    fun getAllRecipes(): Flow<List<Recipe>>
 
     @Insert
-    suspend fun insert(vararg recipes: Recipe)
+    fun insert(vararg recipes: Recipe)
 
     @Delete
-    suspend fun delete(recipe: Recipe)
+    fun delete(recipe: Recipe)
 
     @Query("DELETE FROM recipe")
-    suspend fun deleteAll()
+    fun deleteAll()
 }
